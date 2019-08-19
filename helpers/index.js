aaa3267eb9479ea87d51fd64bac7b29243957f02
nickfoden <nickfoden@gmail.com>
2019-08-19 16:30:03 -0400

    Change let to const

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,5 @@
 exports.mapToObj = arrayOfObjs => {
-  let resultObj = {};
+  const resultObj = {};
   arrayOfObjs.map(item => {
     if (item.ref && item.ref.id && item.data) {
       resultObj[item.ref.id] = item.data;

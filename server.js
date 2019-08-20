6d9f993ea10a2bc51c24ca2ed524358a87c97b8a
Nick Foden <nickfoden@gmail.com>
2019-08-20 12:19:25 -0400

    Allow all requests in production temporarily

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const indexRouter = require("./routes/indexRoute");
 
 const { PORT } = require("./config");
 
 const app = express();
+app.use(cors());
 
 app.use("/", indexRouter);
 
